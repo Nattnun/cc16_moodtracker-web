0a11940a42d762e1a56ffddfe9e8ea9df4e3c260
Nattnun <142487291+Nattnun@users.noreply.github.com>
2024-02-13 21:56:15 +0700

    try to get token and set authUser but failed

--- src/features/auth/contexts/AuthContext.jsx
+++ src/features/auth/contexts/AuthContext.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 import { createContext } from "react";
 import * as authApi from "../../../api/auth";
 import { storeToken, getToken, clearToken } from "../../../utils/local-storage";
+import { useEffect } from "react";
 
 export const AuthContext = createContext();
 
 export default function AuthContextProvider({ children }) {
   const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    if (getToken()) {
+      try {
+        const res = authApi.fetchUser();
+        setAuthUser(res.data.user);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
 
   const register = async (user) => {
     const res = await authApi.register(user);
