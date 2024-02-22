f44fbc37019841a8e578c4af9454e789d36c8ccf
Nattnun <142487291+Nattnun@users.noreply.github.com>
2024-02-22 14:29:35 +0700

    textPass

--- src/components/icons/AddIcon.jsx
+++ src/components/icons/AddIcon.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function AddIcon({ className }) {
+export default function AddIcon({ className, fill }) {
   return (
     <svg
       width="55"
       height="55"
       viewBox="0 0 55 55"
-      fill="none"
+      fill={fill}
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
